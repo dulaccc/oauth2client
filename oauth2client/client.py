e1038cad4f0f916af74fe04dfe59b32271c50be4
Pierre Dulac <dulacpier@gmail.com>
2014-12-27 03:36:09 +0100

    Avoid converting to bytes the Python 3 str header values

--- oauth2client/client.py
+++ oauth2client/client.py
@@ -410,8 +410,12 @@ def clean_headers(headers):
   clean = {}
   try:
     for k, v in six.iteritems(headers):
-      clean_k = k if isinstance(k, bytes) else str(k).encode('ascii')
-      clean_v = v if isinstance(v, bytes) else str(v).encode('ascii')
+      if six.PY3:
+        clean_k = str(k)
+        clean_v = str(v)
+      else:
+        clean_k = k if isinstance(k, bytes) else str(k).encode('ascii')
+        clean_v = v if isinstance(v, bytes) else str(v).encode('ascii')
       clean[clean_k] = clean_v
   except UnicodeEncodeError:
     raise NonAsciiHeaderError(k + ': ' + v)
